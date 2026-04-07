adcf501537c1c175263673258cc98f6ca5b767b3
websuraj-prog <surajvishawakarma01@gmail.com>
2026-04-07 14:19:57 +0530

    vite build added

--- backend/server.js
+++ backend/server.js
@@ -86,13 +86,13 @@ app.post("/upload", upload.single("image"), async (req, res) => {
 
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
 
   res.sendFile(
     path.join(__dirname, "../frontend/dist/index.html")
   );
 
-});
+}); 
 
 
 // start server
